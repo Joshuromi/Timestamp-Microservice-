de98ec23606a6e2bd3c7607b91822ebee4ef47b4
Joshuromi <45287264+Joshuromi@users.noreply.github.com>
2019-08-13 09:20:06 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,26 @@
+{
+  "name": "api_projects",
+  "version": "0.0.1",
+  "description": "An FCC backend challenge",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.16.4",
+    "cors": "^2.8.0",
+    "moment": "^2.10.6"
+  },
+  "engines": {
+    "node": "8.x"
+  },
+  "repository": {
+    "url": "https://glitch.com/edit/#!/hello-express"
+  },
+  "license": "MIT",
+  "keywords": [
+    "node",
+    "glitch",
+    "express"
+  ]
+}
